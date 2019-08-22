85cac705157b96cda17abd5505a72afaca7ee445
Randika Prasad Weerasooriya <randikapw@gmail.com>
2019-08-22 11:33:20 +0530

    Create start_kafka_cmd.bat

---
+++ start_kafka_cmd.bat
@@ -0,0 +1,17 @@
+
+TITLE ::: K A F K A Console interface :::
+echo off
+color 0A
+set PATH=%PATH%;C:\Users\rweerasooriya\Downloads\kafka_2.11-2.3.0\bin\windows
+echo %PATH:;=&echo.%
+cls
+
+::Ascii Art for start
+echo ".------..------..------..------..------.";
+echo "|K.--. ||A.--. ||F.--. ||K.--. ||A.--. |";
+echo "| :/\: || (\/) || :(): || :/\: || (\/) |";
+echo "| :\/: || :\/: || ()() || :\/: || :\/: |";
+echo "| '--'K|| '--'A|| '--'F|| '--'K|| '--'A|";
+echo "\`------'\`------'\`------'\`------'\`------'";
+
+cmd /k 
